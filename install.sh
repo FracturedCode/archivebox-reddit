a59f3f9bec3a46d04a1b7da33910c528c3341f96
Gabe C <gabe@fracturedcode.net>
2022-05-17 20:15:46 -0700

    Install script improvements

--- install.sh
+++ install.sh
@@ -17,30 +17,38 @@ get_latest() {
     sed -E 's/.*"([^"]+)".*/\1/'
 }
 
-echog "Downloading and extracting source"
+echog "Downloading and extracting temporary source"
 TAR_FILE=$(get_latest tag_name).tar.gz
+UNZIPPED_TAR_DIR=$TAR_FILE-unzipped
 curl --progress-bar -L -o $TAR_FILE $(get_latest browser_download_url)
-mkdir -p archivebox-reddit
-tar -xf $TAR_FILE -C ./archivebox-reddit
-cd archivebox-reddit
+mkdir -p $UNZIPPED_TAR_DIR
+tar -xf $TAR_FILE -C ./$UNZIPPED_TAR_DIR
+cd $UNZIPPED_TAR_DIR
 ACCOUNT_DETAILS=export-saved-reddit/AccountDetails.py
 
-echog "Downloading dependencies"
+echog "Installing praw via pip"
 pip install -r export-saved-reddit/requirements.txt
 
 echog "Setting up account details"
 cp details_message.txt $ACCOUNT_DETAILS
+echo >> $ACCOUNT_DETAILS
 cat $ACCOUNT_DETAILS.example >> $ACCOUNT_DETAILS
-nano $ACCOUNT_DETAILS
+"${EDITOR:-nano}" $ACCOUNT_DETAILS
 
-#echog "Installing cron job"
-#ARCHIVEBOX_BIN = /home/ArchiveBox/archivebox/bin/
-#cp ${SRC}reddit_saved_imports.sh $ARCHIVEBOX_BIN
-#chmod +x $ARCHIVEBOX_BIN/reddit_saved_imports.sh
-#echo '0 24 * * * archivebox ${ARCHIVEBOX_BIN}reddit_saved_imports.sh' > /etc/cron.d/archivebox_scheduled_reddit_saved_import
-#
-#echog "Install script complete. Check for errors in the output"
-#
+echog "Installing cron job for every 24 hours"
+ARCHIVEBOX_BIN = /home/ArchiveBox/archivebox/bin/archivebox-reddit/
+mkdir -p $ARCHIVEBOX_BIN
+cp reddit_saved_imports.sh $ARCHIVEBOX_BIN
+cp export-saved-reddit/export_saved.py $ARCHIVEBOX_BIN
+cp export-saved-reddit/AccountDetails.py $ARCHIVEBOX_BIN
+echo '0 24 * * * archivebox ${ARCHIVEBOX_BIN}reddit_saved_imports.sh' > /etc/cron.d/archivebox_scheduled_reddit_saved_import
+
+
+echog "Nuking temporary source"
+rm -rf $UNZIPPED_TAR_DIR
+
+echog "Install script complete. This is a dumb script, so check for errors in the output"
+
 
 # TODO think about how logging is setup
 # TODO maybe have a cutoff so archivebox doesn't have to hunt whether or not stuff has already been archived
--- reddit_saved_imports.sh
+++ reddit_saved_imports.sh
@@ -7,6 +7,6 @@
 LOG_FILE = /home/ArchiveBox/archivebox/logs/archivebox-reddit-saved-$(date "%Y-%m-%d").log
 
 echo "Grabbing saved from reddit"
-python export-saved-reddit/export_saved.py --all
+python export_saved.py --all
 echo "Formatting export-saved-reddit output for archivebox. Archivebox log file: $LOG_FILE"
 python format_csv.py | archivebox add >> $LOG_FILE
