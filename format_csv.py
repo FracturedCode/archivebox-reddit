11522d8423d14a40f730ed7e8b9bf0ece94ceaf4
Gabe C <gabe@fracturedcode.net>
2022-11-19 01:37:27 -0700

    make format_csv.py resistant to domain changes

--- format_csv.py
+++ format_csv.py
@@ -5,16 +5,18 @@ import sys
 if __name__=='__main__':
 	saveHistory = {}
 	saveHistoryPath = '/data/archivebox_reddit_save_history.txt'
+	stripDomain = lambda y: re.sub("^https:\/\/.*\/", "", y)
 	try:
 		with open(saveHistoryPath, mode='r') as saveHistoryFile:
-			saveHistory = set(line for line in saveHistoryFile.readlines())
+			saveHistory = set(stripDomain(line) for line in saveHistoryFile.readlines())
 	except FileNotFoundError:
 		pass
 	with open(sys.argv[1], mode='r') as exportFile:
 		csvIter = csv.reader(exportFile)
 		next(csvIter)
-		libreddit = lambda y: re.sub('www.reddit.com', 'libreddit.spike.codes', y, count=1)
-		eligibleSaves = [libreddit(x[0])+'?context=9999' for x in csvIter if len(x) > 0 and not libreddit(x[0]) in saveHistory]
+		contextParam = '?context=9999'
+		libreddit = lambda y: re.sub('^https://www.reddit.com/', 'https://libreddit.spike.codes/', y, count=1) + contextParam
+		eligibleSaves = [libreddit(x[0]) for x in csvIter if len(x) > 0 and not stripDomain(x[0] + contextParam) in saveHistory]
 		with open(saveHistoryPath, mode='a') as saveHistoryFile:
 			if any(eligibleSaves):
 				for y in eligibleSaves:
